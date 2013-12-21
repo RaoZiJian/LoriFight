52d7932f20e74e9cdfaac026a3a23d8fd2076cfe
Ken <callmelina@163.com>
2013-12-21 17:37:21 +0800

    Add CCArmature resources.

--- LoriFight/src/resource.js
+++ LoriFight/src/resource.js
@@ -2,15 +2,18 @@
 fx_blood            = "res/spark.plist";
 
 // ZIJIAN
-s_blood             = "res/UI/blood.png";
-s_blood_vector      = "res/UI/blood-vector.png";
-s_eatBlood          = "res/UI/eatBlood.png";
-s_GameMenuUI_1      = "res/UI/GameMenuUI_1.json";
-s_kill_monster_goal = "res/UI/kill-monster-goal.png";
-s_settingBtn        = "res/UI/settingBtn.png";
-s_settingBtnPush    = "res/UI/settingBtnPush.png";
-s_slider_heart      = "res/UI/slider-heart.png";
-s_labelatlasimg     = "res/UI/GUI/labelatlasimg.png";
+s_blood                 = "res/UI/blood.png";
+s_blood_vector          = "res/UI/blood-vector.png";
+s_eatBlood              = "res/UI/eatBlood.png";
+s_GameMenuUI_1          = "res/UI/GameMenuUI_1.json";
+s_kill_monster_goal     = "res/UI/kill-monster-goal.png";
+s_settingBtn            = "res/UI/settingBtn.png";
+s_settingBtnPush        = "res/UI/settingBtnPush.png";
+s_slider_heart          = "res/UI/slider-heart.png";
+s_labelatlasimg         = "res/UI/GUI/labelatlasimg.png";
+s_CCArmature_ExportJson = "res/Animations/CCArmature.ExportJson";
+s_CCArmature_Plist      = "res/Animations/CCArmature0.plist";
+s_CCArmature_Png        = "res/Animations/CCArmature0.png";
 
 // YUYE
 
@@ -33,6 +36,9 @@ var menu_resources = [
     {src:s_settingBtnPush},
     {src:s_slider_heart},
     {src:s_labelatlasimg},
+    {src:s_CCArmature_ExportJson},
+    {src:s_CCArmature_Plist},
+    {src:s_CCArmature_Png},
     // YUYE
 
     // HUABIN
