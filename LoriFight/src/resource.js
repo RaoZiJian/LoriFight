8dad7b907b1fe24bbcaacec6a140d634f5ae2f7b
Ken <callmelina@163.com>
2013-12-21 22:34:24 +0800

    Add enemy resources

--- LoriFight/src/resource.js
+++ LoriFight/src/resource.js
@@ -26,6 +26,16 @@ s_StickyBuff_Png        = "res/ItemPic/stickyBuff.png";
 s_StickyMushroom_Png    = "res/ItemPic/stickyMushroom.png";
 s_VisibleBuff_Png       = "res/ItemPic/visibleBuff.png";
 s_VisibleMushroom_Png   = "res/ItemPic/visibleMushroom.png";
+s_Slime1_Png            = "res/Animations/slime1.png";
+s_Slime2_Png            = "res/Animations/slime2.png";
+s_WolfBite1_Png         = "res/Animations/wolfBite1.png";
+s_WolfBite2_Png         = "res/Animations/wolfBite2.png";
+s_WolfWalk1_Png         = "res/Animations/wolfWalk1.png";
+s_WolfWalk2_Png         = "res/Animations/wolfWalk2.png";
+s_ZombieAttack3_Png     = "res/Animations/zombieAttack3.png";
+s_ZombieAttack4_Png     = "res/Animations/zombieAttack4.png";
+s_ZombieMove1_Png       = "res/Animations/zombieMove1.png";
+s_ZombieMove2_Png       = "res/Animations/zombieMove2.png";
 
 // YUYE
 
@@ -57,7 +67,16 @@ var menu_resources = [
     {src:s_StickyBuff_Png},
     {src:s_StickyMushroom_Png},
     {src:s_VisibleBuff_Png},
-    {src:s_VisibleMushroom_Png},
+    {src:s_Slime1_Png},
+    {src:s_Slime2_Png},
+    {src:s_WolfBite1_Png},
+    {src:s_WolfBite2_Png},
+    {src:s_WolfWalk1_Png},
+    {src:s_WolfWalk2_Png},
+    {src:s_ZombieAttack3_Png},
+    {src:s_ZombieAttack4_Png},
+    {src:s_ZombieMove1_Png},
+    {src:s_ZombieMove2_Png},
     // YUYE
 
     // HUABIN
