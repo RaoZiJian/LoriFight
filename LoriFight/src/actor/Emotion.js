74fa2e3ae39f44f5a018339477106a17ea571b25
pandamicro <lphuabin@gmail.com>
2013-12-21 19:05:32 +0800

    Add emotion

--- LoriFight/src/actor/Emotion.js
+++ LoriFight/src/actor/Emotion.js
@@ -1,3 +1,10 @@
 /**
  * Created by panda on 12/20/13.
  */
+
+var Emotion = cc.Sprite.extend({
+    ctor: function(emotionfile, rect) {
+        this._super();
+        this.initWithFile(emotionfile, rect);
+    }
+});
