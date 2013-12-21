9eafe5ff52ec99e819b14beaa937cb93dced41ce
Ken <callmelina@163.com>
2013-12-22 01:09:51 +0800

    Add expressions.

--- LoriFight/src/UILayer/GameUILayer.js
+++ LoriFight/src/UILayer/GameUILayer.js
@@ -21,13 +21,13 @@ var GameUILayer = ccs.UILayer.extend({
     score:null,
     widget:null,
     angerExpression:null,
-    angerFireNumber:null,
     angerFire:null,
+    winSize:null,
 
     init:function(){
 
         if(this._super()){
-            var winSize = cc.Director.getInstance().getWinSize();
+            this.winSize = cc.Director.getInstance().getWinSize();
     //        this.addWidget(ccs.GUIReader.getInstance().widgetFromJsonFile(s_GameMenuUI_1));
             this.widget = ccs.GUIReader.getInstance().widgetFromJsonFile(s_GameMenuUI_1);
             this.addWidget(this.widget);
@@ -35,13 +35,7 @@ var GameUILayer = ccs.UILayer.extend({
             //this.settingBtn    = this.getWidgetByName("settingBtn");
             this.bloodBar      = this.getWidgetByName("blood");
             this.distanceScore = this.getWidgetByName("Score");
-            this.angerExpression = cc.Sprite.create(s_Anger1_Png);
-            this.angerExpression.setAnchorPoint(cc.p(0,0));
-            this.angerExpression.setPosition(cc.p(winSize.width/4, winSize.height/6));
-            this.angerExpression.setScale(0.6);
-            this.angerExpression.setOpacity(200);
-            this.angerFireNumber = 2;
-            this.addChild(this.angerExpression);
+            this.angerExpression = this.setAngerExpression(50);
 
             //this.settingBtn.addTouchEventListener(this.settingBtnCallback, this);
             this.setBloodBarPercent(100);
@@ -51,30 +45,47 @@ var GameUILayer = ccs.UILayer.extend({
 
     setAngerExpression: function(angerValue){
 
-        if(angerValue>=100){
-            this.angerExpression.setTexture(s_Anger2_Png);
-            this.angerFireNumber = 3;
-        }else if(300>angerValue>=200){
-            this.angerExpression.setTexture(s_Anger3_Png);
-            this.angerFireNumber = 4;
-        }else if(angerValue>=300){
-            this.angerExpression.setTexture(s_Anger4_Png);
-            this.angerFireNumber = 5;
-        }else if(angerValue<=50){
-            this.angerExpression.setTexture(s_Anger5_Png);
-            this.angerFireNumber = 1;
-        }else if(50<angerValue<100){
-            this.angerExpression.setTexture(s_Anger1_Png);
+        if(this.angerFire!=null){
+            for(var i=0;i<this.angerFire.length;i++){
+
+                this.angerFire[i].removeFromParent(true);
+                this.angerFire[i] = null;
+            }
         }
 
+        if(angerValue>=100){
+            this.angerExpression = cc.Sprite.create(s_Anger2_Png);
+            this.angerFire = new Array(3);
+        }else if(300>angerValue>=200){
+            this.angerExpression = cc.Sprite.create(s_Anger3_Png);
+            this.angerFire = new Array(4);
+        }else if(angerValue>=300){
+            this.angerExpression = cc.Sprite.create(s_Anger4_Png);
+            this.angerFire = new Array(5);
+        }else if(angerValue<=50){
+            this.angerExpression = cc.Sprite.create(s_Anger5_Png);
+            this.angerFire = new Array(1);
+        }else if(50<angerValue<100){
+            this.angerExpression = cc.Sprite.create(s_Anger1_Png);
+            this.angerFire = new Array(2);
+        }
+
+        this.angerExpression.setScale(0.6);
+        this.angerExpression.setOpacity(200);
+        this.angerExpression.setAnchorPoint(cc.p(0,0));
+        this.angerExpression.setPosition(cc.p(this.winSize.width/4, this.winSize.height/6));
+        this.addChild(this.angerExpression);
         this.setAngerFire();
     },
 
     setAngerFire:function(){
+        for(var i=0; i<this.angerFire.length;i++){
 
-        for(var i=0; i<this.angerFireNumber;i++){
-
-            this
+            var angerFireSprite = cc.Sprite.create(s_AngerFire_Png);
+            angerFireSprite.setAnchorPoint(cc.p(0,0));
+            angerFireSprite.setPosition(cc.p(this.winSize.width/4 + 55*(i+1), this.winSize.height/6-5));
+            this.addChild(angerFireSprite);
+            this.angerFire[i] = angerFireSprite;
         }
     },
 
